c0bed081c5e527e486398fc43f9bdacdfd8cb6c1
Lee Tierney <leetierney@Lees-MacBook-Pro-2.local>
2023-05-10 20:07:55 +0800

    initial packages, function structure

---
+++ tm_scraping_script.py
@@ -0,0 +1,18 @@
+#import packages
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+#Header to be used in request on site
+headers = {'User-Agent':  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+
+#Function to scrape results
+def scrape_results(league, season):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
